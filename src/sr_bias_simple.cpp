b297540499f03e3db4c3c7d027ccdf0e22036736
Chris Cahill <christopherfishcahill@gmail.com>
2022-10-30 05:25:13 +0100

    bump cpp state space fixes

--- src/sr_bias_simple.cpp
+++ src/sr_bias_simple.cpp
@@ -3,23 +3,26 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
   // Data
-  DATA_VECTOR(ln_RS);        // vector of ln(R/S)
+  DATA_VECTOR(ln_RS);            // vector of R
   DATA_VECTOR(S);            // vector of S
   Type n_t = ln_RS.size(); 
   
   // Parameters
   PARAMETER(log_a);
-  PARAMETER(log_sdwt);      // process sd
-  PARAMETER(log_sdm);       // measurement sd 
   PARAMETER(log_b);
+  PARAMETER(log_sdp);       // process sd
+  PARAMETER(log_sdm);       // measurement sd 
+  PARAMETER(rho);           // ar(1) parameter 
   PARAMETER_VECTOR(wt);     // latent state random coefficients
+  PARAMETER(wt0);           // initial wt state
   
   // Objective function
   Type jnll = 0;
   
   // Probability of random coefficients--wt a vector of latent states
-  for( int t=0; t<n_t; t++){
-    jnll -= dnorm( wt(t), Type(0.0), exp(log_sdwt), true);
+  jnll -= dnorm(wt(0), wt0, exp(log_sdp), true);  
+  for( int t=1; t<n_t; t++){
+    jnll -= dnorm( wt(t), rho*wt(t-1), exp(log_sdp), true); 
   }
   
   // Probability of data conditional on fixed and random effect values
@@ -28,10 +31,9 @@ Type objective_function<Type>::operator() ()
   }
   
   // Reporting
-  Type sdwt = exp(log_sdwt);
+  Type sdp = exp(log_sdp);
   Type sdm = exp(log_sdm);
-  REPORT(wt);
-  REPORT(sdwt);
+  REPORT(sdp);
   REPORT(sdm);
   
   return jnll;
