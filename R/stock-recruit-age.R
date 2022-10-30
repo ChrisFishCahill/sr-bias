5bb5cb79870491ccbd73d70e08e658ec3cc253f0
Chris Cahill <christopherfishcahill@gmail.com>
2022-10-30 10:57:34 +0100

    bump working state space

--- R/stock-recruit-age.R
+++ R/stock-recruit-age.R
@@ -34,7 +34,7 @@ EPRo <- sum(fa * Surv0)
 EPRf <- sum(fa * SurvF)
 reca <- RecK / EPRo
 recb <- -log(1 / RecK) / (Ro * EPRo)
-sdr <- 0.6
+sdr <- 1.0
 rhor <- 0
 sdc <- sqrt(1 - rhor^2) * sdr
 Bo <- Ro * EPRo
@@ -52,7 +52,7 @@ vulb[1] <- sum(nta[1, ] * vul * wa)
 yield[1] <- Ut[1] * sum(nta[1, ] * vul * wa)
 
 # pick some deviates
-set.seed(1)
+#set.seed(1)
 rstd <- rnorm(n_year)
 rdev[1] <- sdc * rstd[1]
 for (t in 2:n_year) rdev[t] <- rhor * rdev[t - 1] + sdc * rstd[t]
@@ -77,28 +77,66 @@ for (t in 2:n_year) {
 S <- nta[1:(n_year - 1), 1]
 ln_RS <- eggs[1:(n_year - 1)] / S
 
-par(mfrow = c(1, 2))
-plot(yield, type = "b")
-plot(vulb, type = "b")
+#par(mfrow = c(1, 1))
+#plot(yield, type = "b")
+#plot(vulb, type = "b")
 plot(ln_RS ~ S)
 
 #-----------------------
+nt = 100
+wo = 3
+sdp = 0.5
+sdm = 0.5
+a = 1
+b = 1
+rho = 0
+
+# Simulate predictors
+# log_rt = rt = rep(NA, nt)
+# log_rt[1] = log_r0
+# for( t in 2:nt ){
+#   log_rt[t] = alpha + beta*log_rt[t-1] + rnorm( 1, mean=0, sd=sigmaP )
+# }
+# for( t in 1:nt ){
+#   log_b_t[t] = log_rt[t] + rnorm( 1, mean=0, sd=sigmaM )
+# }
+
 library(TMB)
 
 cppfile <- "src/sr_bias_simple.cpp"
 compile(cppfile)
 dyn.load(dynlib("src/sr_bias_simple"))
+
+rho_start = 0 
 parameters <- list(
-  "log_a" = log(1), "log_sdwt" = log(1),
-  "log_sdm" = log(0.2),
-  "log_b" = 1, "wt" = rep(0, n_year-1)
+  "ln_a" = log(1), "ln_b" = log(1),
+   "ln_sdp" = log(0.1), "ln_sdm" = log(0.2),
+   logit_rho = exp(rho_start)/(1 + exp(rho_start)), 
+   "wt" = rep(0, n_year-1)
 )
 random <- c("wt")
 
 data <- list("ln_RS" = ln_RS, "S" = S)
 obj <- MakeADFun(data = data, parameters = parameters, random = random)
+
 obj$fn(obj$par)
 obj$gr(obj$par)
 
 opt <- nlminb(start = obj$par, objective = obj$fn, gradient = obj$gr, control = list("trace" = 1))
-exp(unname(opt$par["log_a"]))
+report = obj$report()
+opt$SD = sdreport( obj )
+
+
+reca
+a_ss[i] = exp(unname(opt$par["log_a"]))
+b_ss[i] = exp(unname(opt$par["log_b"]))
+
+plot(as.list(opt$SD, "Estimate")$wt)
+
+par(mfrow=c(1,2))
+hist(a_ss, main = "state space ricker", xlab = "alpha est", breaks = 30,
+     xlim = c(1,5.5))
+abline(v = reca, lwd = 2, col = "blue")
+
+hist(b_ss, main = "state space ricker", xlab = "beta est", breaks = 30)
+abline(v = recb, lwd = 2, col = "blue")
--- src/sr_bias_simple.cpp
+++ src/sr_bias_simple.cpp
@@ -2,37 +2,35 @@
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
-  // Data
   DATA_VECTOR(ln_RS);            
   DATA_VECTOR(S);            
   Type n_t = ln_RS.size(); 
   
-  // Parameters
-  PARAMETER(log_a);
-  PARAMETER(log_b);
-  PARAMETER(log_sdp);       // process sd
-  PARAMETER(log_sdm);       // measurement sd 
-  PARAMETER(rho);           // ar(1) parameter 
-  PARAMETER_VECTOR(wt);     // latent state random coefficients
-  PARAMETER(wt0);           // initial wt state
+  PARAMETER(ln_a);
+  PARAMETER(ln_b);
+  PARAMETER(ln_sdp);                // process sd
+  PARAMETER(ln_sdm);                // measurement sd 
+  PARAMETER(logit_rho);             // ar(1) parameter 
+  PARAMETER_VECTOR(wt);             // latent state random coefficients
+  // PARAMETER(wto); 
+  Type rho = invlogit(logit_rho); 
   
-  // Objective function
   Type jnll = 0;
   
-  // Probability of random coefficients--wt is a vector of latent states
-  jnll -= dnorm(wt(0), wt0, exp(log_sdp), true);  
+  // probability of random coefficients--wt is a vector of latent states
+  jnll -= dnorm(wt(0), ln_a - exp(ln_b)*S(0), exp(ln_sdp), true);  
+  for( int t=1; t<n_t-1; t++){
+    jnll -= dnorm( wt(t), rho*wt(t-1), exp(ln_sdp)/(1-pow(rho,2)), true); 
+  }
+
+  // probability of data conditional on fixed and random effect values
   for( int t=1; t<n_t; t++){
-    jnll -= dnorm( wt(t), rho*wt(t-1), exp(log_sdp), true); 
+    jnll -= dnorm( ln_RS(t), ln_a - exp(ln_b)*S(t) + wt(t), exp(ln_sdm), true );
   }
   
-  // Probability of data conditional on fixed and random effect values
-  for( int t=0; t<n_t; t++){
-    jnll -= dnorm( ln_RS(t), log_a - exp(log_b)*S(t) + wt(t), exp(log_sdm), true );
-  }
-  
-  // Reporting
-  Type sdp = exp(log_sdp);
-  Type sdm = exp(log_sdm);
+  // reporting
+  Type sdp = exp(ln_sdp);
+  Type sdm = exp(ln_sdm);
   REPORT(sdp);
   REPORT(sdm);
   
