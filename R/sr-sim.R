457c0c7c6e8ceefee1dad9906d2f30b65abd2d49
Chris Cahill <christopherfishcahill@gmail.com>
2022-11-02 13:20:11 -0400

    bump progress

--- R/sr-sim.R
+++ R/sr-sim.R
@@ -1,69 +1,70 @@
 # simulate su and peterson
-# xt = ln(St)
-# rt = ln(Rt)
-# ar = ln(alpha)
-# y = xt + vt
-# rt = ar + x(t-k) - b*x(t-k) + w(t-k)
-# xt = ln(exp(rt) - C)
-# need to initialize Sinit 1,2
+# cahill november 2022
+
+
+# Su and peterman escapement data example
+# Ep = c(1.062,
+#       1.179, 1.385, 1.213, 1.176, 1.385, 1.141, 0.944, 1.087, 1.119,
+#       0.966, 0.918, 0.793, 0.775, 0.645, 0.661, 0.548, 0.703, 0.807,
+#       0.792, 0.9, 1.007, 1.222, 1.269, 1.001, 1.108, 1.032, 1.099)
+#
+#
+# C = c(0.165, 0.23, 0.272, 0.34, 0.385, 0.409, 0.508, 0.596,
+#        0.609, 0.669, 0.674, 0.779, 0.708, 0.864, 0.611, 0.658, 0.631,
+#        0.583, 0.548, 0.534, 0.556, 0.513, 0.332, 0.319, 0.235, 0.2)
 
 k <- 2 # age at maturity
-n_year = 28
-ar = 1.1
-a = exp(ar)
-b = 3
+n_year <- 100
+a <- 1.64 # ricker alpha
+ar <- log(a)
+b <- 1
 
 # make some fake catches -- taken from su and peterson
-C <- c(
-  0.165, 0.23, 0.272, 0.34, 0.385, 0.409, 0.508, 0.596,
-  0.609, 0.669, 0.674, 0.779, 0.708, 0.864, 0.611, 0.658, 0.631,
-  0.583, 0.548, 0.534, 0.556, 0.513, 0.332, 0.319, 0.235, 0.2
+Sinit <- rep(1, k)
+E <- S <- rep(NA, n_year)
+C <- mu <- R <- rep(NA, n_year - k) # mu is log space predictor of R
+sdp <- 0.03 # process error
+h <- runif(n_year - k, 0.01, 0.35) # harvest rate
+
+# Initialize S and C
+S[1:k] <- ar / b # R' = S' = ln(a)/b = equilibrium SR
+
+set.seed(1)
+wt <- rnorm((n_year - 2), 0, sd = sdp)
+
+for (t in 1:(n_year - k)) {
+  R[t] <- a * S[t] * exp(-b * S[t] + wt[t])
+  C[t] <- h[t] * R[t]
+  S[t + k] <- R[t] - C[t]
+}
+
+plot(log(R / S[(k + 1):n_year]) ~ S[(k + 1):n_year],
+  ylab = "ln(R/S) vs. S",
+  xlab = "S"
 )
+# plot(R ~ S[(k+1):n_year], "ylab" = "Recruits", xlab = "Stock")
+summary(lm(log(R / S[(k + 1):n_year]) ~ S[(k + 1):n_year]))
 
-Sinit = rep(2, k)
-S = rep(NA, n_year)
-mu <- R <- E <- rep(NA, n_year-k) # mu is log space predictor of R
-sdp = 0.005
-sdo = 0.01
-
-# set up mu, R, and S
-S[1:k] = Sinit # initialize
-for(t in 1:(n_year - k)){
-  mu[t] = ar + log(S[t]) - b*S[t]
-  R[t] = rlnorm(1, exp(mu[t]), sdp) # process noise
-  S[t + k] = R[t] - C[t]
-}
-
-# calculate escapements based on true value 
-# S[t] and lognormal observation noise:
-for(t in 1:n_year){
-  E[t] = rlnorm(1, exp(log(S[t])), sdo) # obs. noise
-}
+# calculate observed values based on values from process component of model
+sdo <- 0.1 # observation error
+vt <- rnorm(n_year, 0, sdo)
+E <- S * exp(vt) # obs. noise
 
 # TMB
 library(TMB)
+
 data <- list(
-  "k" = 2, 
-  "E" = c(
-    1.062,
-    1.179, 1.385, 1.213, 1.176, 1.385, 1.141, 0.944, 1.087, 1.119,
-    0.966, 0.918, 0.793, 0.775, 0.645, 0.661, 0.548, 0.703, 0.807,
-    0.792, 0.9, 1.007, 1.222, 1.269, 1.001, 1.108, 1.032, 1.099
-  ),
-  "C" = c(
-    0.165, 0.23, 0.272, 0.34, 0.385, 0.409, 0.508, 0.596,
-    0.609, 0.669, 0.674, 0.779, 0.708, 0.864, 0.611, 0.658, 0.631,
-    0.583, 0.548, 0.534, 0.556, 0.513, 0.332, 0.319, 0.235, 0.2
-  )
+  "k" = k,
+  "E" = E,
+  "C" = C
 )
 par <- list(
-  "ar" = log(1.4),
-  "b" = 0.5,
-  "ln_Sinit" = rep(log(1),2),
-  "ln_sdp" = log(0.005),
-  "ln_sdo" = log(0.01), 
-  "ln_R" = log(R)
-  )
+  "ar" = ar,
+  "b" = b,
+  "ln_sdo" = log(sdo),
+  "ln_sdp" = log(sdp),
+  "R" = R
+)
 
 cppfile <- "src/ss_ricker.cpp"
 compile(cppfile)
@@ -75,7 +76,20 @@ obj$fn(obj$par)
 obj$gr(obj$par)
 obj$report()
 
-opt <- nlminb(start = obj$par, objective = obj$fn, gradient = obj$gr)
+opt <- nlminb(
+  start = obj$par, objective = obj$fn,
+  gradient = obj$gr
+)
 
+opt <- nlminb(
+  start = opt$par, objective = obj$fn,
+  gradient = obj$gr
+)
+opt$convergence
+opt$objective
+obj$report()
 
+opt$SD <- sdreport(obj)
+opt$SD
 
+plot(obj$report()$`R` ~ R)
--- src/ss_ricker.cpp
+++ src/ss_ricker.cpp
@@ -1,27 +1,8 @@
 /*
-           State-space Ricker stock-recruit model
-              based on Su and Peterson (2012)
-             1 November 2022 - Cahill QFC MSU
- 
-                          Maths: 
- y(t) = ln(E(t)); where E is Escapement
- x(t) = ln(S(t)); where S(t) is "true" number of spawners
- r(t) = ln(Rt); where R(t) is "true" number of recruits
- ar = ln(a); where a is Ricker alpha
- 
- Observation equation: 
- y(t) = x(t) + v(t) for t = 1,...,n_year
- where T is total number of years
- and v(t) ~ Normal(0, sdvt) and thus represents
- lognormal observation/sampling error
- 
- Process equation 1:
- r(t) = ar + x(t-k) - b*exp(x(t-k)) where k is age at maturity
- 
- Process equation 2: 
- x(t) = ln(exp(r(t)) - C(t)) where C(t) is catch, assumed measured perfectly
+   State-space Ricker stock-recruit model
+   based on Su and Peterson (2012)
+            Cahill QFC MSU
  */
-
 #include <TMB.hpp>
 
 template <class Type>
@@ -39,41 +20,37 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
   DATA_INTEGER(k);               // age at maturity 
-  DATA_VECTOR(E);                // escapements
-  DATA_VECTOR(C);                // catches
+  DATA_VECTOR(E);                // escapements - observed with error
+  DATA_VECTOR(C);                // catches - assumed perfectly known 
   int n_year = E.size();         // total number of years
  
   PARAMETER(ar);                 // ln(a)
   PARAMETER(b);                  // ricker b
-  PARAMETER_VECTOR(ln_Sinit);    // initialize the with Sinit vector of .size() = k
   PARAMETER(ln_sdo);             // observation error
   PARAMETER(ln_sdp);             // process error
-  PARAMETER_VECTOR(ln_R);        // ln(Rt)
-  vector<Type> S(n_year);        // spawners
-  vector<Type> mu(n_year-k);     // predictions
-  vector<Type> R(n_year-k); 
-  S.setZero(); mu.setZero(); R.setZero(); 
+  PARAMETER_VECTOR(R);           // recruits
   
-  // initialize state variables
-  for(int t = 0; t < k; t ++){
-    S(t) = exp(ln_Sinit[t]);
-  }
+  // containers
+  vector<Type> S(n_year);        // spawners
+  vector<Type> mu(n_year-k);     // ln(predicted recruits)
+  S.setZero(); mu.setZero(); 
+  
+  Type sdo = exp(ln_sdo); 
+  Type sdp = exp(ln_sdp); 
+  Type arec = exp(ar); 
+  
+  for(int t = 0; t < k; t ++){ S(t) = ar / b; } // initialize 
   
   Type jnll = 0;
   
   for(int t = 0; t < n_year - k; t++){
-    mu(t) = ar + log(S(t)) - b*S(t); 
-    jnll -= dlnorm(ln_R(t), mu(t), exp(ln_sdp), true);          // process error 
-    R(t) = exp(ln_R(t)); 
+    mu(t) = ar + log(S(t)) - b*S(t);  
     S(t + k) = R(t) - C(t); 
+    jnll -= dlnorm(R(t), mu(t), exp(ln_sdp), true); // process error 
   }
 
   for(int t = 0; t < n_year; t ++){
-    jnll -= dlnorm(E(t), S(t), exp(ln_sdo), true);              // observation error
-  }
-  
-  Type sdo = exp(ln_sdo); 
-  Type sdp = exp(ln_sdp); 
+    jnll -= dlnorm(E(t), exp(S(t)), exp(ln_sdo), true);  // observation error
   
   REPORT(S); 
   REPORT(R); 
@@ -82,6 +59,7 @@ Type objective_function<Type>::operator() ()
   REPORT(E); 
   REPORT(sdp); 
   REPORT(sdo); 
+  REPORT(arec); 
 
   return jnll;
 }
